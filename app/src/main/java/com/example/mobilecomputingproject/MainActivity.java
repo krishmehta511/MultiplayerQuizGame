065cbe10c3ea2f40d83b39410aab8aa794e8fd2e
Krish Mehta <98233063+krishmehta511@users.noreply.github.com>
2023-01-24 23:48:56 +0530

    Added fb conn and identification based on username

--- app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingproject/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.mobilecomputingproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title;
+    Button play_btn;
+    Button leaderboard_btn;
+    Button settings_btn;
+    EditText user_name;
+    Button create_user;
+    TextView warning_text;
+
+    String player_name = "";
+    FirebaseDatabase database;
+    DatabaseReference playerRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +44,10 @@ public class MainActivity extends AppCompatActivity {
         //Hide StatusBar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        TextView title = findViewById(R.id.AppName);
-        Button play_btn = findViewById(R.id.create_room);
-        Button leaderboard_btn = findViewById(R.id.leaderboard);
-        Button settings_btn = findViewById(R.id.settings);
+        title = findViewById(R.id.AppName);
+        play_btn = findViewById(R.id.create_room);
+        leaderboard_btn = findViewById(R.id.leaderboard);
+        settings_btn = findViewById(R.id.settings);
 
         String title_html = "<font color=#ffffff>CompQuiz()</font><font color=#006b38> {</font>";
         String play_html = "<font color=#ffffff>play</font><font color=#006b38> ;</font>";
@@ -33,6 +59,66 @@ public class MainActivity extends AppCompatActivity {
         leaderboard_btn.setText(Html.fromHtml(leaderboard_html, Html.FROM_HTML_MODE_COMPACT));
         settings_btn.setText(Html.fromHtml(settings_html, Html.FROM_HTML_MODE_COMPACT));
 
+        database = FirebaseDatabase.getInstance("https://mobilecomputingproject-d70e0-default-rtdb.asia-southeast1.firebasedatabase.app");
 
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        player_name = preferences.getString("player_name", "");
+        if(player_name.equals("")){
+            showDialog();
+        }
+
+        play_btn.setOnClickListener(view ->{
+            startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+            finish();
+        });
+    }
+
+    private void showDialog(){
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.signin_dialog);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        user_name = dialog.findViewById(R.id.username);
+        create_user = dialog.findViewById(R.id.button2);
+        warning_text = dialog.findViewById(R.id.warning_text);
+
+        create_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player_name = user_name.getText().toString();
+                if(!player_name.equals("")){
+                    database.getReference().child("players/").child(player_name).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists()){
+                                warning_text.setText("*Username already exists.");
+                                user_name.setText("");
+                            } else {
+                                addNewPlayer();
+                                dialog.dismiss();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(MainActivity.this, "Error :(", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    warning_text.setText("*Enter valid username.");
+                }
+            }
+        });
+        dialog.show();
+    }
+
+    private void addNewPlayer(){
+        playerRef = database.getReference("players/" + player_name);
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("player_name", player_name);
+        editor.apply();
+        playerRef.setValue("");
     }
 }
